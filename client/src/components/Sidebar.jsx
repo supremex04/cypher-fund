384c3bfc3d64c159c7ca5eada0bd9ea1a9de7cc8
supremex04 <bhandarisupreme7@gmail.com>
2024-05-04 14:49:19 +0545

    typos

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,11 +7,12 @@ const Icon = ({styles, name, imgUrl, isActive, disabled, handleClick}) => (
   // template string
   <div className={`w-[48px] h-[48px] rounded-[10px] 
   ${isActive && isActive === name && "bg-[#2c2f33]"} flex justify-center
-  items-center ${!disabled && "cursor-pointer"} ${styles}`}>
+  items-center ${!disabled && "cursor-pointer"} ${styles}`} onClick={handleClick}>
       {!isActive ? 
           (<img src = {imgUrl} alt = "fund_logo" className ="w-1/2 h-1/2"/>)
       : 
-        <img src = {imgUrl} alt = "fund_logo" className ="w-1/2 h-1/2"/>
+        <img src = {imgUrl} alt = "fund_logo" className ={`w-1/2 h-1/2
+         ${isActive !== name && 'grayscale'}`}/>
        }
   </div>
 )
