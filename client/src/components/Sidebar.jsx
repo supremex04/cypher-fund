a7136e2004382742fb73542570190dd496b7a098
supremex04 <bhandarisupreme7@gmail.com>
2024-05-04 14:13:11 +0545

    sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Link, useNavigate} from "react-router-dom";
+import {logo, sun} from "../assets";
+import {navlinks} from "../constants";
 
+const Icon = ({styles, name, imgUrl, isActive, disabled, handleClick}) => (
+  // template string
+  <div className={`w-[48px] h-[48px] rounded-[10px] 
+  ${isActive && isActive === name && "bg-[#2c2f33]"} flex justify-center
+  items-center ${!disabled && "cursor-pointer"} ${styles}`}>
+      {!isActive ? 
+          (<img src = {imgUrl} alt = "fund_logo" className ="w-1/2 h-1/2"/>)
+      : 
+        <img src = {imgUrl} alt = "fund_logo" className ="w-1/2 h-1/2"/>
+       }
+  </div>
+)
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
   return (
-    <div>
-      
+    <div className = "flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+      <Link to = "/">
+        <Icon styles = "w-[52px] h-[52px] bg-[#2c2f32]" imgUrl = {logo}/>
+      </Link>
+       
     </div>
   )
 }
