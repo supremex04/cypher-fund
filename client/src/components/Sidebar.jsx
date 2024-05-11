1ec419104a985394b9aab9f38cc9c11a491a5742
supremex04 <bhandarisupreme7@gmail.com>
2024-05-11 11:28:41 +0545

    favicon

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Link, useNavigate} from "react-router-dom";
-import {logo, sun} from "../assets";
+import {favicon, sun} from "../assets";
 import {navlinks} from "../constants";
 
 const Icon = ({styles, name, imgUrl, isActive, disabled, handleClick}) => (
@@ -22,7 +22,7 @@ const Sidebar = () => {
   return (
     <div className = "flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link to = "/">
-        <Icon styles = "w-[52px] h-[52px] bg-[#2c2f32]" imgUrl = {logo}/>
+        <Icon styles = "w-[52px] h-[52px] bg-[#2c2f32]" imgUrl = {favicon}/>
       </Link>
       <div className='flex-1 flex flex-col justify-between items-center 
       bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12'>
